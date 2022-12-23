038d1626048b26dad79306aec8f43f8737d005a3
fadi <fadi@shopee.com>
2022-12-23 14:46:00 +0700

    feat: add modal component

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PrimaryButton, SimpleButton } from '../../components/Button';
+import Modal from '../../components/Modal/Modal';
+import useModal from '../../hooks/modal';
 import { ping } from '../../services/ping';
 import colors from '../../styles/colors';
 
@@ -16,29 +18,33 @@ const AtypDisplayFont = styled.div`
 type Props = Record<string, never>;
 
 const LandingPage: React.FC<Props> = () => {
+  const { showModal, openModal, closeModal } = useModal();
   return (
-    <Container>
-      <AtypDisplayFont>
-        <h1 style={{ backgroundColor: colors.black, color: colors.white }}>
-          digidate
-        </h1>
-        <p style={{ fontWeight: 400 }}>Regular font weight.</p>
-        <p style={{ fontWeight: 500 }}>Medium font weight.</p>
-        <p style={{ fontWeight: 700 }}>Bold font weight.</p>
-      </AtypDisplayFont>
-      <div style={{ marginBottom: '0.5rem' }}>
-        <p style={{ fontWeight: 400 }}>Regular font weight.</p>
-        <p style={{ fontWeight: 500 }}>Medium font weight.</p>
-        <p style={{ fontWeight: 700 }}>Bold font weight.</p>
-      </div>
-      <PrimaryButton onClick={ping}>Ping!</PrimaryButton>
-      <SimpleButton onClick={ping}>Ping!</SimpleButton>
-      <div style={{ backgroundColor: 'gray' }}>
-        <SimpleButton outlined onClick={ping}>
-          Ping!
-        </SimpleButton>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <AtypDisplayFont>
+          <h1 style={{ backgroundColor: colors.black, color: colors.white }}>
+            digidate
+          </h1>
+          <p style={{ fontWeight: 400 }}>Regular font weight.</p>
+          <p style={{ fontWeight: 500 }}>Medium font weight.</p>
+          <p style={{ fontWeight: 700 }}>Bold font weight.</p>
+        </AtypDisplayFont>
+        <div style={{ marginBottom: '0.5rem' }}>
+          <p style={{ fontWeight: 400 }}>Regular font weight.</p>
+          <p style={{ fontWeight: 500 }}>Medium font weight.</p>
+          <p style={{ fontWeight: 700 }}>Bold font weight.</p>
+        </div>
+        <PrimaryButton onClick={ping}>Ping!</PrimaryButton>
+        <SimpleButton onClick={ping}>Ping!</SimpleButton>
+        <div style={{ backgroundColor: 'gray' }}>
+          <SimpleButton outlined onClick={openModal}>
+            Ping!
+          </SimpleButton>
+        </div>
+      </Container>
+      <Modal show={showModal} closeModal={closeModal}></Modal>
+    </>
   );
 };
 
