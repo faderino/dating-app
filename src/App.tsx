2612af2b29e7dfc73164a4cb9db76fa9dff47a68
fadi <fadi@shopee.com>
2022-12-25 15:58:03 +0700

    feat: add rolebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Swipe from './pages/Swipe/Swipe';
 import ProtectedRoutes from './ProtectedRoutes';
+import { RoleID } from './types/user';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route element={<ProtectedRoutes />}>
+      <Route element={<ProtectedRoutes allowedRole={RoleID.User} />}>
         <Route path="/app">
           <Route index element={<Swipe />} />
         </Route>
--- src/ProtectedRoutes.tsx
+++ src/ProtectedRoutes.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAppSelector } from './hooks/store';
+import { useGetUserQuery } from './services/auth';
 import { selectAuthState } from './store/auth/authSlice';
 
-const ProtectedRoutes: React.FC = () => {
+type Props = {
+  allowedRole: number;
+};
+
+const ProtectedRoutes: React.FC<Props> = ({ allowedRole }) => {
   const location = useLocation();
   const { isAuthenticated, token } = useAppSelector(selectAuthState);
+  const { data: user, isLoading } = useGetUserQuery();
 
   if (!isAuthenticated || !token) {
     return <Navigate to="/" state={{ from: location }} replace />;
   }
-  return <Outlet />;
+  if (!isLoading && user?.data.role_id !== allowedRole) {
+    return <Navigate to="/" replace />;
+  }
+  return isLoading ? null : <Outlet />;
 };
 
 export default ProtectedRoutes;
