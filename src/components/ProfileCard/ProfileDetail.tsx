ea85d753897c28a95852b9f784bee07420ca4c5b
fadi <fadi@shopee.com>
2022-12-28 21:15:55 +0700

    feat: click to view next photo

---
+++ src/components/ProfileCard/ProfileDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Profile } from '../../types/profile';
+
+type Props = {
+  profile?: Profile;
+  hideDetails: () => void;
+};
+
+const ProfileDetails: React.FC<Props> = () => {
+  return <div></div>;
+};
+
+export default ProfileDetails;
