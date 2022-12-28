19383aa05e92875acfa2d9b3b454d7adb2243cd3
fadi <fadi@shopee.com>
2022-12-28 23:58:05 +0700

    feat: add static button for like and skip

--- src/pages/Recommendations/Recommendations.tsx
+++ src/pages/Recommendations/Recommendations.tsx
@@ -4,18 +4,22 @@ import { Content } from '../../components/Layout';
 import styled from 'styled-components';
 import { useGetRecommendationsQuery } from '../../services/recommendations';
 import { Profile } from '../../types/profile';
+import { IoMdClose, IoMdHeart } from 'react-icons/io';
+import colors from '../../styles/colors';
 
 const PageContent = styled(Content)`
   height: 100vh;
+  display: flex;
+  flex-direction: column;
   @media screen and (min-width: 896px) {
     max-width: 500px;
     margin: 0 auto;
   }
 `;
 
-const StackList = styled.div`
+const Stack = styled.div`
   width: 98%;
-  height: 99%;
+  flex: 1;
   margin: auto;
   position: relative;
 `;
@@ -24,6 +28,39 @@ const StackItem = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+`;
+
+const ActionButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem 0;
+`;
+
+const ActionButton = styled.button`
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  padding: 0.75rem;
+  transition: transform 0.3s ease;
+  :hover,
+  :hover svg {
+    transform: scale(1.075);
+  }
+`;
+
+const LikeButton = styled(ActionButton)`
+  color: ${colors.primary};
+  border: 2px solid ${colors.primary};
+`;
+
+const SkipButton = styled(ActionButton)`
+  color: ${colors.gray50};
+  border: 2px solid ${colors.gray50};
 `;
 
 const Recommendations: React.FC = () => {
@@ -53,13 +90,22 @@ const Recommendations: React.FC = () => {
 
   return (
     <PageContent>
-      <StackList>
+      <Stack>
         {stack.map((profile) => (
           <StackItem key={profile.profile_id}>
             <ProfileCard profile={profile} handleSwipe={handleSwipe} />
           </StackItem>
         ))}
-      </StackList>
+      </Stack>
+      <ActionButtons>
+        <SkipButton onClick={handleSwipe}>
+          <IoMdClose size={32} />
+        </SkipButton>
+        <p>or</p>
+        <LikeButton onClick={handleSwipe}>
+          <IoMdHeart size={32} />
+        </LikeButton>
+      </ActionButtons>
     </PageContent>
   );
 };
