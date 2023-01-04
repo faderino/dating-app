6d22cc7894f11c4c04ea65f3640e9cb3156c8a1e
fadi <fadi@shopee.com>
2023-01-04 13:48:32 +0700

    feat: remove credentials true

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -3,7 +3,6 @@ import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.REACT_APP_API_URL,
-  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
